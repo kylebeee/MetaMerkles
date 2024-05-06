67a504df25c939c32333ad1e446308d373a89551
Kyle Breeding <kyle.breeding@algorand.foundation>
2024-05-05 20:07:59 -0700

    Project initialised with AlgoKit CLI using template: https://github.com/algorand-devrel/tealscript-algokit-template.git

---
+++ projects/MetaMerkles/__test__/MetaMerkles.test.ts
@@ -0,0 +1,49 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import * as algokit from '@algorandfoundation/algokit-utils';
+import { MetaMerklesClient } from '../contracts/clients/MetaMerklesClient';
+
+const fixture = algorandFixture();
+algokit.Config.configure({ populateAppCallResources: true });
+
+let appClient: MetaMerklesClient;
+
+describe('MetaMerkles', () => {
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { testAccount } = fixture.context;
+    const { algorand } = fixture;
+
+    appClient = new MetaMerklesClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: 0,
+      },
+      algorand.client.algod
+    );
+
+    await appClient.create.createApplication({});
+  });
+
+  test('sum', async () => {
+    const a = 13;
+    const b = 37;
+    const sum = await appClient.doMath({ a, b, operation: 'sum' });
+    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  });
+
+  test('difference', async () => {
+    const a = 13;
+    const b = 37;
+    const diff = await appClient.doMath({ a, b, operation: 'difference' });
+    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  });
+
+  test('hello', async () => {
+    const diff = await appClient.hello({ name: 'world!' });
+    expect(diff.return?.valueOf()).toBe('Hello, world!');
+  });
+});
